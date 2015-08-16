f6c76f1594fb5888e8732c29783528b9ac6bdb9c
Ruwan Geeganage <ruwang@Ruwans-MacBook-Pro.local>
2015-08-17 01:31:01 +0530

    Added Global config functionality

---
+++ fw/configuration/RestifyConfig.js
@@ -0,0 +1,51 @@
+/**
+ * Created by ruwang on 7/9/15.
+ */
+module.exports = {
+  AuthFunction : function(dependencies){
+    /*Add the authentication function as shown below
+     dependencies.filters.<Authentication Filter Module name>.<function name>
+
+     eg:
+     dependencies.filters.Auth.validateAuthHeader
+
+     if there are no Auth function,
+     return false;
+     */
+   return dependencies.filters.SampleFilters.sampleAuthFilter;
+  },
+  AppConfig : function(dependencies){
+
+    return {
+    /*
+     ADD more restify create server parameters,
+
+     eg:
+       certificate: ...,
+       key: ...,
+
+       */
+      name: dependencies.packageJson.name,
+      port : GlobalConfig.Common.port,
+      address : GlobalConfig.Common.address
+    }
+  },
+  RestifyMiddlewareSet : function(dependencies) {
+    return [
+        /*
+        Add more restify modules
+         */
+      dependencies.restifyObject.acceptParser(dependencies.serverObject.acceptable),
+      dependencies.restifyObject.gzipResponse(),
+      dependencies.restifyObject.queryParser(),
+      dependencies.restifyObject.bodyParser()
+    ]
+  },
+  RestifyPre : function(dependencies) {
+    return [
+        /*
+        Add Restify pre() functions
+         */
+    ];
+  }
+}
