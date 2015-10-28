e2688429ca7c297838d42450d2c4c96ba17c3bf5
Ruwan Geeganage <ruwangeeganage@1805-Ruwan-Geeganage.local>
2015-10-28 11:30:00 -0500

    Added main.js

---
+++ main.js
@@ -0,0 +1,4 @@
+console.log("This framework is completely based on Restify for API development.");
+console.log("Please visit https://github.com/ever-js/ever-js,");
+console.log("To learn how to use ever-js");
+console.log("Thank you");
